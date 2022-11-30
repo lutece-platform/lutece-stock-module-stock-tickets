76c4bc906a670452c50aa98f7bc1def9af0a0080
olivier.petit <olivier.petit@host.docker.internal>
2022-11-30 18:51:31 +0100

    mvn format license

--- src/java/fr/paris/lutece/plugins/stock/modules/tickets/service/IShowService.java
+++ src/java/fr/paris/lutece/plugins/stock/modules/tickets/service/IShowService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2018, Mairie de Paris
+ * Copyright (c) 2002-2019, Mairie de Paris
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -74,7 +74,7 @@ public interface IShowService
      */
     ShowDTO getProduct( int nIdProduct );
 
-    Product getProductById(Integer id);
+    Product getProductById( Integer id );
 
     /**
      * Return a filtered list of product.
